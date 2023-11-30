35227ea255f4b96a340ba1a896b67a43cf21c3cf
ayush0355 <ayushsharma3081@gmail.com>
2023-11-30 13:54:28 +0530

    Stack_Valid_Parentheses.java

---
+++ Stack_Valid_Parentheses.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            if(st.size()==0){
+                st.push(s.charAt(i));
+                continue;
+            }
+            if(s.charAt(i)=='{' || s.charAt(i)=='[' || s.charAt(i)=='('){
+                st.push(s.charAt(i));
+            }
+            if(s.charAt(i)==')'){
+                if(st.peek()=='('){
+                    st.pop();}
+                else{ return false;}   
+            }
+            else if(s.charAt(i)==']'){
+                if(st.peek()=='['){
+                    st.pop();}
+                else{return false;}
+            }
+            else if(s.charAt(i)=='}'){
+                if(st.peek()=='{'){
+                    st.pop();}
+                else{return false;}
+            }   
+        }
+        if (st.size()==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
