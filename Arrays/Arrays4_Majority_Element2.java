2b99be2fa2a45807d81d37c01cf4630fc8b821a7
ayush0355 <ayushsharma3081@gmail.com>
2024-01-07 23:20:56 +0530

    Arrays4_Majority_Element2

---
+++ Arrays/Arrays4_Majority_Element2.java
@@ -0,0 +1,52 @@
+https://leetcode.com/problems/majority-element-ii/
+
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        //boyer moore  voiting algorithm//
+        List<Integer> ans=new ArrayList<>();
+        int val1=nums[0];
+        int count1=1;
+        int val2=0;
+        int count2=0;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]==val1){
+                count1++;
+            }
+            else if(nums[i]==val2){
+                count2++;
+            }
+            else{
+                if(count1==0){
+                    val1=nums[i];
+                    count1=1;
+                }
+                else if(count2==0){
+                    val2=nums[i];
+                    count2=1;
+                }
+                else{
+                    count1--;
+                    count2--;
+                }
+            }
+        }
+        int c1=0;
+        int c2=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==val1){
+                c1++;
+            }
+            else if(nums[i]==val2){
+                c2++;
+            }
+        }
+        if(c1>nums.length/3){
+            ans.add(val1);
+        }
+        if(c2>nums.length/3){
+            ans.add(val2);
+        }
+        return ans;
+    }
+}
