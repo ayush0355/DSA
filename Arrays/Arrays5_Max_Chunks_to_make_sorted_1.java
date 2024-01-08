f1653e370ac7348633c22d82c6d743362f5c73d9
ayush0355 <ayushsharma3081@gmail.com>
2024-01-08 22:07:11 +0530

    Arrays/Arrays5_Max_Chunks_to_make_sorted_1

---
+++ Arrays/Arrays5_Max_Chunks_to_make_sorted_1.java
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/max-chunks-to-make-sorted/description/
+
+
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int max=Integer.MIN_VALUE;
+        int count=0;
+        for(int i=0; i<arr.length; i++){
+            max=Math.max(max,arr[i]);
+            if(max==i){
+                count++;
+            }
+        }
+        return count;
+    }
+}
